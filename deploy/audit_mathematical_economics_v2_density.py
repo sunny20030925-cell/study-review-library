579a1404dab3231d7a88d4d1800b2e8b30dffa3c
sunny20030925-cell <sunny20030925@gmail.com>
2026-07-30 04:17:43 +0800

    Add mathematical economics v2 density diagnostic

---
+++ deploy/audit_mathematical_economics_v2_density.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+from __future__ import annotations
+
+import re
+
+import generate_mathematical_economics as legacy
+from mathematical_economics_v2_corrections import CHAPTERS_V2
+
+
+def main() -> None:
+    legacy.CHAPTERS = CHAPTERS_V2
+    lengths = []
+    for chapter in CHAPTERS_V2:
+        html = legacy.chapter_html(chapter)
+        plain = re.sub(r'<[^>]+>', '', html)
+        lengths.append((chapter['id'], len(plain), chapter['title']))
+    for cid, length, title in lengths:
+        print(f'{cid} chars={length} title={title}')
+    thin = [(cid, length) for cid, length, _ in lengths if length < 700]
+    print(f'MATHEMATICAL_ECONOMICS_V2_DENSITY thin_under_700={thin} min={min(x[1] for x in lengths)} max={max(x[1] for x in lengths)}')
+
+
+if __name__ == '__main__':
+    main()
